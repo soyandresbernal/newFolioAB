4ed781fccac666020f142d2e7772035181820ef2
Andres bernal <soyandresbernal@gmail.com>
2022-12-17 19:30:55 -0500

    change config & create porfolio.js

---
+++ src/Components/portfolio.js
@@ -0,0 +1,15 @@
+const header = {
+  homepage: '',
+  title: 'js',
+};
+const about = {
+  name: 'Andrés Bernal',
+  role: 'front Engineer',
+  description:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  resume: 'https://github.com/',
+  social: {
+    linkedin: 'https://linkedin.com',
+    github: 'https://github.com',
+  },
+};
