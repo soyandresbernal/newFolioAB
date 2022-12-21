06c5dc1ccc546e1266b6c7eee5f201778339ad0b
Andres bernal <soyandresbernal@gmail.com>
2022-12-21 11:18:49 -0500

    About Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
+import { useContext } from 'react';
+import { ThemeContext } from './contexts/theme';
+import Header from './components/Header/Header';
+import About from './components/About/About';
+import Projects from './components/Projects/Projects';
+import Skills from './components/Skills/Skills';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
-function App() {
+import './App.css';
+
+const App = () => {
+  const [{ themeName }] = useContext(ThemeContext);
+
   return (
-    <div className='App'>
-      <div>
-        <p>data</p>
-      </div>
+    <div id='top' className={`${themeName} app`}>
+      <Header />
+
+      <main>
+        <About />
+        <Projects />
+        <Skills />
+        <Contact />
+      </main>
+
+      <ScrollToTop />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
