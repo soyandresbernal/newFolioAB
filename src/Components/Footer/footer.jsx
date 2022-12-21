4a18cff22d301ca650213a3e8df37e2ad0628485
Andres bernal <soyandresbernal@gmail.com>
2022-12-21 11:24:26 -0500

    Component Contact Header

---
+++ src/Components/Footer/footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => (
+  <footer className='footer'>
+    <a href='' className='link footer__link'>
+      Created By Andres bernal
+    </a>
+  </footer>
+);
+
+export default Footer;
